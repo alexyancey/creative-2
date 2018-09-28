8a36e8b24d2ceff95660062df4b8b4317a471b1c
alexyancey <james.alexander.yancey@gmail.com>
2018-09-28 16:25:37 +0000

    ip test

---
+++ script.js
@@ -0,0 +1,9 @@
+var url = "http://api.ipstack.com/check?access_key=24578be4e2001fd8c4b7877ae8963ded";
+
+$(document).ready(function()
+{
+    $.getJSON(url, function(data)
+    {
+        console.log(data);
+    });
+});
