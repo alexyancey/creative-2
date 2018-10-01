a7e810bffa5013f225e6413af2302b98b6d65429
ju5tf0rfun <43225791+ju5tf0rfun@users.noreply.github.com>
2018-10-01 10:03:22 -0600

    update

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var url = "http://api.ipstack.com/check?access_key=24578be4e2001fd8c4b7877ae8963ded";
 
-var url2 = "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyCVijDuPyI5oyNin-wH7wpuXRBdRSxXtkQ"
+var googleAPI="AIzaSyCVijDuPyI5oyNin-wH7wpuXRBdRSxXtkQ";
+var url2 = "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyCVijDuPyI5oyNin-wH7wpuXRBdRSxXtkQ";
 
 $(document).ready(function()
 {
@@ -8,7 +9,6 @@ $(document).ready(function()
     {
         console.log(data);
     });
-    
     $.getJSON(url2, function(data)
     {
         console.log(data);
