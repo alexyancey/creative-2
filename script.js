5472b9448a4ea72241c5b0a11252f48115b32136
alexyancey <james.alexander.yancey@gmail.com>
2018-10-01 16:41:50 +0000

    functionality

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 var url = "http://api.ipstack.com/check?access_key=24578be4e2001fd8c4b7877ae8963ded";
-var url2 = "http://api.ipstack.com/52.25.212.185?access_key=24578be4e2001fd8c4b7877ae8963ded";
 
 $(document).ready(function()
 {
@@ -23,21 +22,30 @@ $(document).ready(function()
         
         console.log(output);
     });
-    $.getJSON(url2, function(data)
+    $("#lookupButton").click(function(e)
     {
-        console.log(data);
-        var city = data['city'];
-        var state = data['region_name'];
-        var country = data['country_name'];
-        var zip = data['zip'];
-        var latitude = data['latitude'];
-        var longitude = data['longitude'];
-        var flag = data['location']['country_flag'];
+        var ip = $("#awsIP").val();
+        e.preventDefault();
+        var url2 = "http://api.ipstack.com/" + ip + "?access_key=24578be4e2001fd8c4b7877ae8963ded";
         
-        var output = "Your AWS server is being hosted in " + city + ", " + state + " " + zip + ", " +
-        country + " Latitude: " + latitude + " Longitude: " + longitude;
-        
-        $("#awsIPinfo").html(output);
-        console.log(output);
+        $.getJSON(url2, function(data)
+        {
+            console.log(data);
+            var city = data['city'];
+            var state = data['region_name'];
+            var country = data['country_name'];
+            var zip = data['zip'];
+            var latitude = data['latitude'];
+            var longitude = data['longitude'];
+            var flag = data['location']['country_flag'];
+            
+            var output = "Your AWS server is being hosted in " + city + ", " + state + " " + zip + ", " +
+            country + " Latitude: " + latitude + " Longitude: " + longitude;
+            
+            $("#awsIPinfo").html(output);
+            $("#flag2").attr("src", flag);
+            
+            console.log(output);
+        });
     });
 });
