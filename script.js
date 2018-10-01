2c49f8538613303c647f05be1ec19eab24949704
alexyancey <james.alexander.yancey@gmail.com>
2018-10-01 16:31:18 +0000

    javascript

--- script.js
+++ script.js
@@ -3,12 +3,35 @@ var url2 = "http://api.ipstack.com/52.25.212.185?access_key=24578be4e2001fd8c4b7
 
 $(document).ready(function()
 {
+    //City, region_name, country_name, zip, latitude, longitude
     $.getJSON(url, function(data)
     {
         console.log(data);
+        var city = data['city'];
+        var state = data['region_name'];
+        var country = data['country_name'];
+        var zip = data['zip'];
+        var latitude = data['latitude'];
+        var longitude = data['longitude'];
+        var flag = data['location']['country_flag'];
+        
+        var output = "You are currently in " + city + ", " + state + " " + zip + ", " +
+        country + " Latitude: " + latitude + " Longitude: " + longitude;
+        console.log(output);
     });
     $.getJSON(url2, function(data)
     {
         console.log(data);
+        var city = data['city'];
+        var state = data['region_name'];
+        var country = data['country_name'];
+        var zip = data['zip'];
+        var latitude = data['latitude'];
+        var longitude = data['longitude'];
+        var flag = data['location']['country_flag'];
+        
+        var output = "Your AWS server is being hosted in " + city + ", " + state + " " + zip + ", " +
+        country + " Latitude: " + latitude + " Longitude: " + longitude;
+        console.log(output);
     });
 });
